04d9eebd307dedd6ad472eee3bdba2efd07f0fcd
xgill <38523490+xgill@users.noreply.github.com>
2019-07-29 17:04:09 +0200

    Feature/enquiry (#1)
    * intermediate commit
    
    * intermediate 2
    
    * bumped version 0.0.1

---
+++ src/components/InsureeItemEligibility.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withTheme, withStyles } from "@material-ui/core/styles";
+import { Paper, Grid, Typography } from "@material-ui/core";
+import { FormattedMessage, ProgressOrError, PublishedComponent } from "@openimis/fe-core";
+import { fetchItemEligibility } from "../actions";
+import Eligibility from "./Eligibility";
+
+const styles = theme => ({
+    paper: {
+        margin: theme.spacing(1),
+        marginRight: 0,
+    },
+    header: theme.table.title,
+});
+
+
+class InsureeItemEligibility extends Component {
+
+    onItemSelected = i => {
+        this.props.fetchItemEligibility(i.code);
+    }
+
+    render() {
+        const { classes, fetchingItemEligibility, fetchedItemEligibility, insureeItemEligibility, errorItemEligibility } = this.props;
+
+        return (
+            <Paper className={classes.paper}>
+                <Grid container alignItems="center">
+                    <Grid item xs={4}>
+                        <Typography className={classes.header}>
+                            <FormattedMessage module="policy" id="insureeEligibility.item" />
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <PublishedComponent
+                            id="medical.ItemSimpleSearcher"
+                            onItemSelected={this.onItemSelected}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {fetchingItemEligibility && (
+                            <ProgressOrError progress={fetchingItemEligibility} error={errorItemEligibility} />
+                        )}
+                        {fetchedItemEligibility && (
+                            <Eligibility eligibility={{
+                                "minDate": insureeItemEligibility.minItemDate,
+                                "left": insureeItemEligibility.itemLeft,
+                                "isOk": insureeItemEligibility.isItemOk
+                            }} />
+                        )}
+                    </Grid>
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    insureeItemEligibility: state.policyInsuree.insureeItemEligibility,
+    fetchingItemEligibility: state.policyInsuree.fetchingItemEligibility,
+    fetchedItemEligibility: state.policyInsuree.fetchedItemEligibility,
+    errorItemEligibility: state.policyInsuree.errorItemEligibility,
+});
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ fetchItemEligibility }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    withTheme(withStyles(styles)(InsureeItemEligibility))
+);
---
+++ src/components/InsureeServiceEligibility.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withTheme, withStyles } from "@material-ui/core/styles";
+import { Paper, Grid, Typography } from "@material-ui/core";
+import { FormattedMessage, ProgressOrError, PublishedComponent } from "@openimis/fe-core";
+import { fetchServiceEligibility } from "../actions";
+import Eligibility from "./Eligibility";
+
+const styles = theme => ({
+    paper: {
+        margin: theme.spacing(1),
+        marginRight: 0,
+    },
+    header: theme.table.title,
+});
+
+
+class InsureeServiceEligibility extends Component {
+
+    onServiceSelected = i => {
+        this.props.fetchServiceEligibility(i.code);
+    }
+
+    render() {
+        const { classes, fetchingServiceEligibility, fetchedServiceEligibility, insureeServiceEligibility, errorServiceEligibility } = this.props;
+
+        return (
+            <Paper className={classes.paper}>
+                <Grid container alignItems="center">
+                    <Grid item xs={4}>
+                        <Typography className={classes.header}>
+                            <FormattedMessage module="policy" id="insureeEligibility.service" />
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <PublishedComponent
+                            id="medical.ServiceSimpleSearcher"
+                            onServiceSelected={this.onServiceSelected}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {fetchingServiceEligibility && (
+                            <ProgressOrError progress={fetchingServiceEligibility} error={errorServiceEligibility} />
+                        )}
+                        {fetchedServiceEligibility && (
+                            <Eligibility eligibility={{
+                                "minDate": insureeServiceEligibility.minServiceDate,
+                                "left": insureeServiceEligibility.serviceLeft,
+                                "isOk": insureeServiceEligibility.isServiceOk
+                            }} />
+                        )}
+                    </Grid>
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    insureeServiceEligibility: state.policyInsuree.insureeServiceEligibility,
+    fetchingServiceEligibility: state.policyInsuree.fetchingServiceEligibility,
+    fetchedServiceEligibility: state.policyInsuree.fetchedServiceEligibility,
+    errorServiceEligibility: state.policyInsuree.errorServiceEligibility,
+});
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ fetchServiceEligibility }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    withTheme(withStyles(styles)(InsureeServiceEligibility))
+);
