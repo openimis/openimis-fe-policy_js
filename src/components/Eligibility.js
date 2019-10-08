525636f736afdfb8eb2f2148b5bd465ced4305ec
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-08 23:16:54 +0200

    naming conv

--- src/components/Eligibility.js
+++ src/components/Eligibility.js
@@ -12,19 +12,19 @@ class Eligibility extends Component {
     render() {
         const { modulesManager, eligibility } = this.props;
         let itemFormatters = [];
-        if (!modulesManager.skipControl("policy", "insureeEligibility.minDate")) {
+        if (!modulesManager.hideField("policy", "insureeEligibility.minDate")) {
             itemFormatters.push(
                 i => <FieldLabel module="policy" id="insureeEligibility.minDate" />,
                 i => !!i.minDate ? <FormattedDate value={i.minDate} /> : null
             );
         }
-        if (!modulesManager.skipControl("policy", "insureeEligibility.left")) {
+        if (!modulesManager.hideField("policy", "insureeEligibility.left")) {
             itemFormatters.push(
                 i => <FieldLabel module="policy" id="insureeEligibility.left" />,
                 i => i.left
             );
         }
-        if (!modulesManager.skipControl("policy", "insureeEligibility.isOk")) {
+        if (!modulesManager.hideField("policy", "insureeEligibility.isOk")) {
             itemFormatters.push(i => i.isOk ? <ThumbUp /> : <ThumbDown />);
         }
 
