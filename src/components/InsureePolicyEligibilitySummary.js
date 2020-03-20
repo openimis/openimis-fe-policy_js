e8b9e2ca94c1606b5f386410a48b7489fcacd9eb
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-03-20 08:54:46 +0100

    OMT-161: wrong policy data in claim form & bumped 1.0.5

--- src/components/InsureePolicyEligibilitySummary.js
+++ src/components/InsureePolicyEligibilitySummary.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from "redux";
 import { injectIntl } from 'react-intl';
 import { withTheme, withStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
-import { ProgressOrError, PublishedComponent, FormattedMessage, AmountInput } from "@openimis/fe-core";
+import { ProgressOrError, PublishedComponent, FormattedMessage, AmountInput, TextInput } from "@openimis/fe-core";
 import { fetchPolicies } from "../actions";
 
 import { ACTIVE_POLICY_STATUS } from "../constants";
@@ -15,15 +15,47 @@ const styles = theme => ({
 
 class InsureePolicyEligibilitySummary extends Component {
 
+    state = {
+        insureePolicies: []
+    }
+
     componentDidMount() {
         if (this.props.insuree) {
             this.props.fetchPolicies(this.props.insuree.chfId);
+        } else {
+            this.setState({ insureePolicies: [] })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (!!prevProps.insuree && !this.props.insuree) {
+            this.setState({ insureePolicies: [] })
+            return;
+        }
+        if ((!prevProps.insuree && !!this.props.insuree) ||
+            !!prevProps.insuree && !!this.props.insuree &&
+            (
+                prevProps.insuree.chfId == null
+                || prevProps.insuree.chfId !== this.props.insuree.chfId
+            )
+        ) {
+            this.setState(
+                { insureePolicies: [] },
+                e => {
+                    this.props.fetchPolicies(this.props.insuree.chfId)
+                }
+            )
+            return;
+        }
+        if (!prevProps.fetchedPolicies && this.props.fetchedPolicies) {
+            this.setState({ insureePolicies: this.props.insureePolicies })
         }
     }
 
     render() {
         const { classes, fetchingPolicies, fetchedPolicies, errorPolicies } = this.props;
-        var activePolicy = !!this.props.insureePolicies && this.props.insureePolicies.filter(p => p.status === ACTIVE_POLICY_STATUS).pop();
+        const { insureePolicies } = this.state;
+        var activePolicy = !!insureePolicies && insureePolicies.filter(p => p.status === ACTIVE_POLICY_STATUS).pop();
         return (
             <Fragment>
                 <ProgressOrError progress={fetchingPolicies} error={errorPolicies} />
@@ -35,7 +67,23 @@ class InsureePolicyEligibilitySummary extends Component {
                 {!!fetchedPolicies && !!activePolicy && (
                     <Grid item>
                         <Grid container>
-                            <Grid item xs={6} className={classes.item}>
+                            <Grid item xs={2} className={classes.item}>
+                                <TextInput
+                                    value={activePolicy.productCode}
+                                    module="policy"
+                                    label="policy.insureePolicies.productCode"
+                                    readOnly={true}
+                                />
+                            </Grid>
+                            <Grid item xs={4} className={classes.item}>
+                                <TextInput
+                                    value={activePolicy.productName}
+                                    module="policy"
+                                    label="policy.insureePolicies.productName"
+                                    readOnly={true}
+                                />
+                            </Grid>
+                            <Grid item xs={3} className={classes.item}>
                                 <PublishedComponent id="core.DatePicker"
                                     value={activePolicy.expiryDate}
                                     module="policy"
@@ -43,12 +91,13 @@ class InsureePolicyEligibilitySummary extends Component {
                                     readOnly={true}
                                 />
                             </Grid>
-                            <Grid item xs={6} className={classes.item}>
+                            <Grid item xs={3} className={classes.item}>
                                 <AmountInput
-                                    value={(activePolicy.ceiling || 0) - (activePolicy.ded || 0)}
+                                    value={0}
                                     module="claim"
                                     label="balance"
                                     readOnly={true}
+                                    displayZero={true}
                                 />
                             </Grid>
                         </Grid>
@@ -60,7 +109,6 @@ class InsureePolicyEligibilitySummary extends Component {
 }
 
 const mapStateToProps = state => ({
-    insuree: !!state.claim.claim ? state.claim.claim.insuree : null,
     fetchingPolicies: state.policy.fetchingInsureePolicies,
     fetchedPolicies: state.policy.fetchedInsureePolicies,
     insureePolicies: state.policy.insureePolicies,
