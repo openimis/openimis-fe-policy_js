ca917d79ceb087af300a91bb0c1c183c487cf28f
Quentin Gérôme <geromequentin@gmail.com>
2021-10-26 23:20:43 +0200

    perf: Rewrite PolicyOfficerPicker

--- src/pickers/PolicyOfficerPicker.js
+++ src/pickers/PolicyOfficerPicker.js
@@ -1,86 +1,63 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { withTheme, withStyles } from "@material-ui/core/styles";
-import { injectIntl } from 'react-intl';
-import { fetchPolicyOfficers } from "../actions";
-import { formatMessage, AutoSuggestion, ProgressOrError, withModulesManager, decodeId } from "@openimis/fe-core";
+import React, { useState } from "react";
+import { useTranslations, Autocomplete, useGraphqlQuery } from "@openimis/fe-core";
 
-const styles = theme => ({
-    label: {
-        color: theme.palette.primary.main
-    }
-});
+const PolicyOfficerPicker = (props) => {
+  const {
+    onChange,
+    readOnly,
+    required,
+    withLabel = true,
+    withPlaceholder,
+    value,
+    label,
+    filterOptions,
+    filterSelectedOptions,
+    placeholder,
+    extraFragment,
+    multiple,
+  } = props;
+  const [searchString, setSearchString] = useState(null);
+  const { formatMessage } = useTranslations("policy");
 
-class PolicyOfficerPicker extends Component {
-
-    constructor(props) {
-        super(props);
-        this.selectThreshold = props.modulesManager.getConf("fe-policy", "PolicyOfficer.selectThreshold", 10);
-    }
-
-    componentDidMount() {
-        if (!this.props.fetchedPolicyOfficers) {
-            // prevent loading multiple times the cache when component is
-            // several times on tha page
-            setTimeout(
-                () => {
-                    !this.props.fetchingPolicyOfficers && this.props.fetchPolicyOfficers(this.props.modulesManager)
-                },
-                Math.floor(Math.random() * 300)
-            );
+  const { isLoading, data, error } = useGraphqlQuery(
+    `query PolicyOfficerPicker ($searchString: String, $first: Int) {
+      policyOfficers(search: $searchString, first: $first) {
+        edges {
+          node {
+            id
+            uuid
+            code
+            lastName
+            otherNames
+            ${extraFragment ?? ""}
+          } 
         }
-    }
+      }
+    }`,
+    { searchString, first: 20 },
+    { skip: true }
+  );
 
-    formatSuggestion = a => !a ? "" : `${a.code} ${a.lastName} ${a.otherName || ""}`;
-
-    onSuggestionSelected = v => this.props.onChange(v, this.formatSuggestion(v));
-
-    render() {
-        const {
-            intl, value, reset, policyOfficers,
-            fetchingPolicyOfficers, fetchedPolicyOfficers, errorPolicyOfficers,
-            withLabel = true, label, readOnly = false, required = false,
-            withNull = false, nullLabel = null,
-        } = this.props;
-        let v = policyOfficers ? policyOfficers.filter(o => !!value && o.id === value.id) : [];
-        v = v.length ? v[0] : null;
-        return (
-            <Fragment>
-                <ProgressOrError progress={fetchingPolicyOfficers} error={errorPolicyOfficers} />
-                {fetchedPolicyOfficers && (
-                    <AutoSuggestion
-                        module="policy"
-                        items={policyOfficers}
-                        label={!!withLabel && (label || formatMessage(intl, "policy", "PolicyOfficerPicker.label"))}
-                        getSuggestions={this.policyOfficers}
-                        getSuggestionValue={this.formatSuggestion}
-                        onSuggestionSelected={this.onSuggestionSelected}
-                        value={v}
-                        reset={reset}
-                        readOnly={readOnly}
-                        required={required}
-                        selectThreshold={this.selectThreshold}
-                        withNull={withNull}
-                        nullLabel={nullLabel || formatMessage(intl, "policy", "policy.PolicyOfficerPicker.null")}
-                    />
-                )}
-            </Fragment>
-        )
-    }
-}
-
-const mapStateToProps = state => ({
-    policyOfficers: state.policy.policyOfficers,
-    fetchingPolicyOfficers: state.policy.fetchingPolicyOfficers,
-    fetchedPolicyOfficers: state.policy.fetchedPolicyOfficers,
-    errorPolicyOfficers: state.policy.errorPolicyOfficers,
-});
-
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ fetchPolicyOfficers }, dispatch);
+  return (
+    <Autocomplete
+      multiple={multiple}
+      required={required}
+      placeholder={placeholder ?? formatMessage("PolicyOfficerPicker.placeholder")}
+      label={label ?? formatMessage("PolicyOfficerPicker.label")}
+      error={error}
+      withLabel={withLabel}
+      withPlaceholder={withPlaceholder}
+      readOnly={readOnly}
+      options={data?.policyOfficers?.edges.map((edge) => edge.node) ?? []}
+      isLoading={isLoading}
+      value={value}
+      getOptionLabel={(option) => `${option.code} ${option.lastName} ${option.otherNames}`}
+      onChange={(option) => onChange(option, option ? `${option.code} ${option.lastName} ${option.otherNames}` : null)}
+      filterOptions={filterOptions}
+      filterSelectedOptions={filterSelectedOptions}
+      onInputChange={setSearchString}
+    />
+  );
 };
 
-export default withModulesManager(
-    connect(mapStateToProps, mapDispatchToProps)(injectIntl(withTheme(withStyles(styles)(PolicyOfficerPicker))))
-);
+export default PolicyOfficerPicker;
