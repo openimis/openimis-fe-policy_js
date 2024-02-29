3ba883bfbf6573e823a482e926e95d313228fccd
Oliver Lewandowski <109145288+olewandowski1@users.noreply.github.com>
2024-02-29 09:35:20 +0100

    OP-1823: add payer dropdown when paying in one installment (#85)
    * OP-1823: add payer dropdown when paying in one installment
    
    * OP-1823: make payer not required
    
    * OP-1823: remove required prop

--- src/components/PolicyMasterPanel.js
+++ src/components/PolicyMasterPanel.js
@@ -426,6 +426,15 @@ class PolicyMasterPanel extends FormPanel {
                   </Grid>
                   <Grid item xs={3} className={classes.item}>
                     <PublishedComponent
+                      pubRef="payer.PayerPicker"
+                      withNull={true}
+                      readOnly={readOnly}
+                      value={edited?.payer}
+                      onChange={(p) => this.updateAttribute("payer", p)}
+                    />
+                  </Grid>
+                  <Grid item xs={3} className={classes.item}>
+                    <PublishedComponent
                       pubRef="core.DatePicker"
                       module="contribution"
                       value={edited?.enrollDate}
