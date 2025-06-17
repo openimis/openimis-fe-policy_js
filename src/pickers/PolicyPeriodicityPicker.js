84d2d56d71d363230137e2fab0e9482ae0539668
michellewis <kouamkouammichellewis@gmail.com>
2025-06-17 10:35:09 -0400

    feat: definition des valeurs minimales pour les periodicités

--- src/pickers/PolicyPeriodicityPicker.js
+++ src/pickers/PolicyPeriodicityPicker.js
@@ -6,12 +6,23 @@ import { PERIODICITY_VALUES } from "../constants";
 class PolicyPeriodicityPicker extends Component {
 
     render() {
-        return <ConstantBasedPicker
-            module="policy"
-            label="Periodicity"
-            constants={PERIODICITY_VALUES}
-            {...this.props}
-        />
+        const { contributionPlan } = this.props;
+
+        // Logique de filtrage sans modifier la constante d'origine
+        const isAmos = !!contributionPlan && ['AMOS1', 'AMOS2', 'AMOS3', 'AMOS4'].includes(contributionPlan.code);
+        const isAms = !!contributionPlan && contributionPlan.code === "AMS";
+        const filteredPeriodicity = isAmos
+            ? PERIODICITY_VALUES.filter(p => p !== "M")
+            : isAms ? PERIODICITY_VALUES.filter(p => p!== "M" && p!== "Q" && p!== "S") : PERIODICITY_VALUES
+        return (
+            <ConstantBasedPicker
+                module="policy"
+                label="Periodicity"
+                constants={filteredPeriodicity}
+                value={filteredPeriodicity[0]}
+                {...this.props}
+            />
+        );
     }
 }
 
